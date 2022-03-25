aec2b76b25c33ad4d3ea1b14f4920911ac8f7873
Kevin W Edwards <kevin.w.edwards@gmail.com>
2022-03-25 14:38:05 -0500

    init commit

---
+++ fakestoreapi/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace FakeStore.Console
+{
+    public class Rating
+    {
+        public double rate { get; set; }
+        public int count { get; set; }
+    }
+
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+        public Rating rating { get; set; }
+    }
+}
---
+++ fakestoreapi/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FakeStore.Console;
+using Newtonsoft.Json;
+
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri($"https://fakestoreapi.com/");
+
+var response = await client.GetAsync("products");
+string json = await response.Content.ReadAsStringAsync();
+
+List<Product>? products = JsonConvert.DeserializeObject<List<Product>>(json);
+
+List<Product>? highlyratedProducts = products?.Where(t => t.rating.rate > 3)
+                                    .OrderByDescending(t => t.rating.rate)
+                                    .ToList();
+
+//highlyratedProducts = null;
+
+if (highlyratedProducts!=null && highlyratedProducts.Any())
+{
+    foreach (var product in highlyratedProducts)
+    {
+        Console.WriteLine($"{product.rating.rate}: {product.title}");
+    }
+}
+else
+{
+    Console.WriteLine($"No highly rated items exist");
+}
